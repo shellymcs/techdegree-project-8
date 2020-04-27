3555888ce0f86907fdfdef9b2d3cb55d4626dcc1
shellymcs <61569144+shellymcs@users.noreply.github.com>
2020-04-28 10:02:58 +1200

    modal

--- js/script.js
+++ js/script.js
@@ -11,6 +11,7 @@
   const mainGrid = document.querySelector('.grid-container');
   const card = document.querySelector('.card');
   let employData =[];
+  
 
   fetch(API)
       .then(checkStatus)
@@ -28,11 +29,11 @@
     employData.forEach((employee, index) => {
       employeeHTML += `
       <div class='card' data-index="${index}">
-      <img class='photo' src='${employee.picture.large}>
+      <img class='photo' src='${employee.picture.large}'>
       <div class="main-info">
       <h2 class="name">${employee.name.first} ${employee.name.last}</h2>
       <p class="email">${employee.email}</p>
-      <p class="address">${employee.city}</p>
+      <p class="address">${employee.location.state}</p>
       </div>
      </div>
      `
@@ -45,3 +46,39 @@
       if (response.ok){return Promise.resolve(response);
   }else{return Promise.reject(newError(response.statusText));
 }}
+
+//=====Modal========
+
+const overlay = document.querySelector('.overlay');
+const modalCard = document.querySelector('.modal-card');
+const modalClose  = document.querySelector('.modal-close');
+const modal  = document.querySelector('.modal-container');
+
+card.addEventListener('click', e =>{
+  overlay.classList.remove("hidden");
+   let modalHTML= '';
+  employData.forEach((employee, index) => {
+    modalHTML += `
+    <div class='modalCard' data-index="${index}">
+    <img class='photo' src='${employee.picture.large}'>
+    <div class="modal-info">
+    <h2 class="name">${employee.name.first} ${employee.name.last}</h2>
+    <p class="email">${employee.email}</p>
+    <p class="address">${employee.location.state}</p>
+    </div>
+    <div class="extraInfo">
+    <p class="phone">${employee.phone}</p>
+    <p class="full-address">${employee.location.street}, ${employee.location.city}, ${employee.location.postcode}</p>
+    <p class="dob">Birthday: ${employee.dob.date}</p>
+    
+    </div>
+   </div>
+   `
+  });
+
+   modal.innerHTML = modalHTML;
+});
+
+modalClose.addEventListener('click', e =>{
+  overlay.classList.add("hidden");
+})
